d314086b805f46913fa90450c53ec1c2fc0bce43
alxdb <alxdb@pm.me>
2024-10-08 19:18:48 +0100

    use zed-editor package

--- modules/zed.nix
+++ modules/zed.nix
@@ -1,11 +1,4 @@
 { pkgs, ... }:
-let
-  zed-fhs = pkgs.buildFHSUserEnv {
-    name = "zed";
-    targetPkgs = pkgs: with pkgs; [ zed-editor ];
-    runScript = "zed";
-  };
-in
 {
-  home.packages = [ zed-fhs ];
+  home.packages = [ pkgs.zed-editor ];
 }
