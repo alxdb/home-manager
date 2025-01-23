5d6365a058f825be77fcf113f13c0b013bd19d65
alxdb <alxdb@pm.me>
2025-01-23 08:41:10 +0000

    Mostly broken zed configuration.

--- modules/zed.nix
+++ modules/zed.nix
@@ -1,4 +1,42 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages = [ pkgs.zed-editor.fhs ];
+  programs.zed-editor = {
+    enable = true;
+    extensions = [
+      "nix"
+      "toml"
+      "make"
+      "catpuccin"
+    ];
+    userSettings = {
+      theme = {
+        mode = "system";
+        dark = "Catpuccin Mocha";
+      };
+      node = {
+        path = lib.getExe pkgs.nodejs;
+        npm_path = lib.getExe' pkgs.nodejs "npm";
+      };
+      lsp = {
+        rust-analyzer = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+        json-language-server = {
+          binary = {
+            path = lib.getExe pkgs.nodePackages.vscode-json-languageserver;
+            arguments = "--stdio";
+          };
+        };
+      };
+      vim_mode = true;
+      load_direnv = "shell_hook";
+    };
+  };
 }
